695669f75da06bad7c443fb619ed070caaca46df
paul <ibw1953@naver.com>
2026-01-19 22:49:45 +0900

    가상키패드 화면만 적용 복호화 기능 추가전

---
+++ vue_sample/src/components/transkey/TranskeyEdit.vue
@@ -0,0 +1,18 @@
+<template>
+    <input :type="type" :id="id" :name="name" :data-tk-kbdtype="keypadType" :placeholder="placeholder" @click="showKeyboard($event.target)"/>
+</template>
+
+<script setup>
+import {defineProps} from 'vue';
+
+const showKeyboard = (inputObj) => {
+    window.tk.onKeyboard(inputObj);
+}
+defineProps({
+    type:String,
+    id:String,
+    name: String,
+    keypadType: String,
+    placeholder: String
+})
+</script>
--- vue_sample/src/components/transkey/TranskeySample.vue
+++ vue_sample/src/components/transkey/TranskeySample.vue
@@ -1,10 +1,20 @@
 <template>
     <div>
         <h1>TranskeySample.vue</h1>
+        <div>
+            <TranskeyEdit type='text' id='qwertykeypad' name='qwertykeypad' keypadType='qwerty' placeholder='qwertykeyapd' isDecodeBtn='true'/><br/>
+            <TranskeyEdit type='password' id='numberkeypad' name='numberkeypad' keypadType='number' placeholder='numberkeypad' isDecodeBtn='true'/>
+        </div>
+        <h2>initTranskey() 함수 호출이후 동적으로 input 생성될 경우 tk.setKeyboard함수를 사용합니다</h2>
     </div>
 </template>
-<script>
-export default ({
-    name: 'TranskeySample'
+<script setup>
+import { onMounted } from 'vue';
+import TranskeyEdit from './TranskeyEdit.vue';
+onMounted(()=>{
+    window.tk_useTranskey = true;
+    setTimeout(()=>{
+        window.initTranskey();
+    }, 300);
 })
 </script>
