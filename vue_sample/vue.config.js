303814daaf5120d2dea16c0c2bbfe3ffe9700f2a
paul <ibw1953@naver.com>
2026-01-19 22:26:57 +0900

    프록시설정

--- vue_sample/vue.config.js
+++ vue_sample/vue.config.js
@@ -6,7 +6,6 @@ module.exports = defineConfig({
       '/api': {
         target: 'http://localhost:8080', // 백엔드 서버의 실제 주소
         changeOrigin: true,                // CORS 회피를 위해 호스트 헤더 변경
-        pathRewrite: { '^/api': '' }       // URL에서 /api 부분을 제거하고 백엔드에 전달
       },
       '/transkeyServlet': {
         target: 'http://localhost:8080', // 백엔드 서버의 실제 주소
